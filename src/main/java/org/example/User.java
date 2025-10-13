a9005e05f061122c47dd67ee3bce9d81c43eddc6
sabsiru <zero013@naver.com>
2025-10-13 17:54:14 +0900

    feat: password 검증 클래스

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class User {
+    public String getPassword() {
+        return password;
+    }
+
+    private String password;
+
+    public void initPassword(PasswordGenerator passwordGenerator) {
+        //RandomPasswordGenerator randomPasswordGenerator = new RandomPasswordGenerator();
+        String password = passwordGenerator.generatePassword();
+
+        /**
+         * 비밀번호는 최소 8자 이상 12자 이하여야 한다.
+         */
+
+        if (password.length() >= 8 && password.length() <= 12) {
+            this.password = password;
+        }
+    }
+}
