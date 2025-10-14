898d638bf5456ebb43cdac00f2b0e3e7a2c40389
sabsiru <zero013@naver.com>
2025-10-14 16:13:07 +0900

    feat: calculate

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.example.calculate.*;
+
+import java.util.List;
+
+public class Calculator {
+
+    private static final List<NewArithmeticOperator> arithmeticOperators = List.of(
+            new AdditionOperator(),
+            new SubtractionOperator(),
+            new MultiplicationOperator(),
+            new DivisionOperator()
+    );
+
+    public static int calculate(PositiveNumber operand1, String operator, PositiveNumber operand2) {
+        return arithmeticOperators.stream()
+                .filter(arithmeticOperator -> arithmeticOperator.supports(operator))
+                .map(arithmeticOperator -> arithmeticOperator.calculate(operand1, operand2))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("올바른 연산자가 아닙니다."));
+
+    }
+}
