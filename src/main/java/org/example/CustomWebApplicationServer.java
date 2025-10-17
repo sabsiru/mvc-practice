015f58656079bc4460ce8a923e688f3c6a33c87e
sabsiru <zero013@naver.com>
2025-10-17 13:56:48 +0900

    fix: 메인Thread 사용x 별도의 스레드를 생성하게 로직 수정

--- src/main/java/org/example/CustomWebApplicationServer.java
+++ src/main/java/org/example/CustomWebApplicationServer.java
@@ -1,17 +1,11 @@
 package org.example;
 
-import org.example.calculate.domain.Calculator;
-import org.example.calculate.domain.PositiveNumber;
-import org.example.http.HttpRequest;
-import org.example.http.HttpResponse;
-import org.example.http.QueryStrings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 
 public class CustomWebApplicationServer {
 
@@ -34,33 +28,7 @@ public class CustomWebApplicationServer {
                 logger.info("[CustomWebApplicationServer] client connected");
 
 
-                /**
-                 * Step1 - 사용자 요청을 메인 Trhead가 처리하도록 한다.
-                 * */
-                try (InputStream in = clientSocket.getInputStream();
-                     OutputStream out = clientSocket.getOutputStream()) {
-                    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-                    DataOutputStream dos = new DataOutputStream(out);
-
-
-                    HttpRequest httpRequest = new HttpRequest(br);
-
-                    //GET /calculate?operand1=11&operator=*&operand2=55 HTTP/1.1
-                    if (httpRequest.isGetRequest() && httpRequest.matchPath("/calculate")) {
-                        QueryStrings queryStrings = httpRequest.getQueryStrings();
-
-                        int operand1 = Integer.parseInt(queryStrings.getValue("operand1"));
-                        String operator = queryStrings.getValue("operator");
-                        int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
-
-                        int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
-                        byte[] body = String.valueOf(result).getBytes();
-
-                        HttpResponse response = new HttpResponse(dos);
-                        response.response200Header("application/json", body.length);
-                        response.responseBody(body);
-                    }
-                }
+                new Thread(new ClientRequestHandler(clientSocket)).start();
             }
         }
     }
