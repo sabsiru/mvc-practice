95df64869e892aad13f541a11cbee2e07002aa2f
sabsiru <zero013@naver.com>
2025-10-29 13:24:58 +0900

    init

---
+++ src/main/java/org/example/mvc/RequestMappingHandlerMapping.java
@@ -0,0 +1,10 @@
+package org.example.mvc;
+
+import org.example.mvc.controller.Controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RequestMappingHandlerMapping {
+    private Map<String, Controller> mappings = new HashMap<>();
+}
