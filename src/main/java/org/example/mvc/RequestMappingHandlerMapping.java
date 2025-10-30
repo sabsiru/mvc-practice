0d14ea202cdfca9fa822192429675f709626d8a7
sabsiru <zero013@naver.com>
2025-10-30 13:23:06 +0900

    front 컨트롤러 패턴

--- src/main/java/org/example/mvc/RequestMappingHandlerMapping.java
+++ src/main/java/org/example/mvc/RequestMappingHandlerMapping.java
@@ -1,5 +1,6 @@
 package org.example.mvc;
 
+import org.example.controller.HomeController;
 import org.example.mvc.controller.Controller;
 
 import java.util.HashMap;
@@ -7,4 +8,12 @@ import java.util.Map;
 
 public class RequestMappingHandlerMapping {
     private Map<String, Controller> mappings = new HashMap<>();
+
+    void init() {
+        mappings.put("/", (Controller) new HomeController());
+    }
+
+    public Controller findHandler(String uriPath) {
+        return mappings.get(uriPath);
+    }
 }
--- src/main/java/org/example/mvc/controller/Controller.java
+++ src/main/java/org/example/mvc/controller/Controller.java
@@ -4,5 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface Controller {
+
     String handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception;
 }
